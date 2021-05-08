eca6916c1ded2cf0c95cf6566a934d76fbbdc798
fernndes <fernndes.gabriel@hotmail.com>
2021-05-08 10:21:11 -0300

    bugs

--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,4 @@ const instance = axios.create({
     baseURL: process.env.REACT_APP_URL
 })
 
-instance.defaults.headers.common['Access-Control-Allow-Origin'] = process.env.REACT_APP_URL
-
 export default instance
